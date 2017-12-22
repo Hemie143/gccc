5564e4d37cfe0efd896b24702e6761035a166802
Laurent Hemeryck <hemie143@gmail.com>
2017-12-22 13:28:29 +0100

    Capture image

--- gccc.py
+++ gccc.py
@@ -1,5 +1,8 @@
 import requests
+from time import time
 from io import open as iopen
+# import cv2
+
 
 url = 'http://geocheck.org/geo_inputchkcoord.php?gid=249971030fb-5db4-45f2-a4ec-2e2312ca63d9'
 
@@ -7,8 +10,7 @@ sess = requests.Session()
 page = sess.get(url)
 
 captcha = sess.get("http://geocheck.org/geocheck_captcha.php")
-captcha_file = "C:/temp/cap.png"
-
+captcha_file = 'pics/cap_{}.png'.format(int(time()))
 
 with iopen(captcha_file, 'wb') as file:
     file.write(captcha.content)
